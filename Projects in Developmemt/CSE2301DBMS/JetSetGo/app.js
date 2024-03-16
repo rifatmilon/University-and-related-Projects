52a9ee8fb29f9c8bcaf8a5096cf8216bce5b2c34
rifatmilon <rifataknda.r@gmail.com>
2024-03-16 16:09:35 +0600

    Explore Section added. v: 0x3x3x24

--- Projects in Developmemt/CSE2301DBMS/JetSetGo/app.js
+++ Projects in Developmemt/CSE2301DBMS/JetSetGo/app.js
@@ -42,3 +42,16 @@ const changeSliderImage = () => {
     }, index * 100);
   })
 }
+
+
+// nav
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >= 188){
+        navbar.classList.add('bg');
+    } else{
+        navbar.classList.remove('bg')
+    }
+})
